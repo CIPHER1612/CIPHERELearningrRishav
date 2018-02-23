c5e0fde29f8dabb6701bcefa7040f4e1d63eeae8
sammy <sampathh123@gmail.com>
2018-02-23 19:26:23 +0530

    manifest

--- app/src/main/java/com/example/sammy/ciphere_learning/MainActivity.java
+++ app/src/main/java/com/example/sammy/ciphere_learning/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.sammy.ciphere_learning;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.CircularProgressDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -15,20 +18,21 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText editTextEmail, editTextPassword,editTextRegNo;
-    String cs="cs";
-    String ec="ec";
-    String ee="ee";
-    String adft="adft";
-    String ce="ce";
-    String me="me";
+    EditText editTextEmail, editTextPassword, editTextRegNo;
+    String cs = "cs";
+    String ec = "ec";
+    String ee = "ee";
+    String adft = "adft";
+    String ce = "ce";
+    String me = "me";
     String ok;
     private FirebaseAuth mAuth;
     private FirebaseFirestore Fstore;
+    private ProgressBar progressBar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Fstore=FirebaseFirestore.getInstance();
+        Fstore = FirebaseFirestore.getInstance();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -37,8 +41,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.regButton).setOnClickListener(this);
         findViewById(R.id.LogButton).setOnClickListener(this);
+        progressBar2 = findViewById(R.id.ppjk);
         mAuth = FirebaseAuth.getInstance();
-
 
 
     }
@@ -47,7 +51,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.regButton:
+                progressBar2.setVisibility(View.VISIBLE);
                 startActivity(new Intent(this, RegActivity.class));
+                progressBar2.setVisibility(View.INVISIBLE);
                 break;
             case R.id.LogButton:
                 userlogin();
@@ -56,66 +62,74 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void userlogin() {
-        String email=editTextEmail.getText().toString().trim();
-        String password=editTextPassword.getText().toString().trim();
+    public void FirebaseAuthInvalidCredentialsException(String code, String message) {
 
-        if(email.isEmpty()){
+    }
+
+    private void userlogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
             editTextEmail.setError("Email is required");
             editTextEmail.requestFocus();
             return;
         }
 
 
-        if(password.length()<6){
+        if (password.length() < 6) {
             editTextPassword.setError(("Minimum length of password is 6 characters"));
             editTextPassword.requestFocus();
             return;
         }
-        if(password.isEmpty()){
+        if (password.isEmpty()) {
             editTextPassword.setError("Password is required");
             editTextPassword.requestFocus();
             return;
         }
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                String usid = mAuth.getCurrentUser().getUid();
 
-                Fstore.collection("Users").document(usid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()){
-                            DocumentSnapshot documentSnapshot=task.getResult();
-                            String depid = documentSnapshot.getString("Deptid").toLowerCase();
-                            if(depid.matches(cs)) {
-                                Intent intent=new Intent(MainActivity.this,CShome.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(intent);
+                if (task.isSuccessful()) {
+                    String usid = mAuth.getCurrentUser().getUid();
 
-                            }else
 
-                            if (depid.matches(me)) {
-                                Intent intent=new Intent(MainActivity.this,Mechhome.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(intent);
+                    Fstore.collection("Users").document(usid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot documentSnapshot = task.getResult();
+                                String depid = documentSnapshot.getString("Deptid").toLowerCase();
+                                if (depid.matches(cs)) {
+                                    Intent intent = new Intent(MainActivity.this, CShome.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(intent);
 
-                            }
-                            else
+                                } else if (depid.matches(me)) {
+                                    Intent intent = new Intent(MainActivity.this, Mechhome.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(intent);
 
-                            if (depid.matches(ce)) {
-                                Intent intent=new Intent(MainActivity.this,CEhome.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(intent);
+                                } else if (depid.matches(ce)) {
+                                    Intent intent = new Intent(MainActivity.this, CEhome.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(intent);
 
+                                }
                             }
                         }
-                    }
-                });
+                    });
+
 
                  /*else{
                     Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
                 }*/
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Invalid Login", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
