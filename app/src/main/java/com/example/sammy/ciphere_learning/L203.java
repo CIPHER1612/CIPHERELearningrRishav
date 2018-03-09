aec41d00655b8e15fb1ec6c7370771ec78ad234c
Grim Genesis <sampathh123@gmail.com>
2018-03-09 20:19:34 +0530

    RISHAV L2L3

---
+++ app/src/main/java/com/example/sammy/ciphere_learning/L203.java
@@ -0,0 +1,13 @@
+package com.example.sammy.ciphere_learning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class L203 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l203);
+    }
+}
---
+++ app/src/main/java/com/example/sammy/ciphere_learning/L301.java
@@ -0,0 +1,13 @@
+package com.example.sammy.ciphere_learning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class L301 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l301);
+    }
+}
