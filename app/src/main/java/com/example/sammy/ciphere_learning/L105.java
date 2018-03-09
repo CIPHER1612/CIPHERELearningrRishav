25eef9cb2f40205a40c15849efd3a2b9409a6c70
Grim Genesis <sampathh123@gmail.com>
2018-03-09 19:43:08 +0530

    RISHAV L1

---
+++ app/src/main/java/com/example/sammy/ciphere_learning/L105.java
@@ -0,0 +1,26 @@
+package com.example.sammy.ciphere_learning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class L105 extends AppCompatActivity implements View.OnClickListener{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l105);
+        findViewById(R.id.ch1p1).setOnClickListener(this);
+        findViewById(R.id.ch1p2).setOnClickListener(this);
+        findViewById(R.id.ch1p3).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+    }
+}
