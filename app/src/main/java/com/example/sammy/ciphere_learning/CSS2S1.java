e2dfbc28c0bc3f0f96c16d981a02e5740b68a2c5
subho <sampathh123@gmail.com>
2018-03-15 19:32:41 +0530

    subho

--- app/src/main/java/com/example/sammy/ciphere_learning/CSS2S1.java
+++ app/src/main/java/com/example/sammy/ciphere_learning/CSS2S1.java
@@ -35,7 +35,7 @@ public class CSS2S1 extends AppCompatActivity {
         css1c1t1.getSettings().setDatabaseEnabled(true);
         css1c1t1.getSettings().setAppCacheEnabled(true);
         css1c1t1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        css1c1t1.loadUrl("https://drive.google.com/open?id=0BweSpKFxiUG9VjJESl9zQ0F0Z1E");
+        css1c1t1.loadUrl("https://drive.google.com/open?id=1HxjdMi4CsT7HGf-1CbvwvDenLCPSZ3yA");
         css1c1t1.setWebViewClient(new WebViewClient());
     }
 }
